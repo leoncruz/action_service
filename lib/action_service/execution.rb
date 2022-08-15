5a1fb4d47ac1323b703965ea0976d99a5f2d3bd9
Leon Cruz <leon.cruz.teixeira@gmail.com>
2022-08-14 21:46:03 -0300

    module for execution of actions

---
+++ lib/action_service/execution.rb
@@ -0,0 +1,36 @@
+# frozen_string_literal: true
+
+require 'action_service/success'
+require 'action_service/faliure'
+
+module ActionService
+  # Module for execute actions from ActionService
+  module Execution
+    def execute(actions_to_execute)
+      actions_to_execute.each do |action|
+        result = send(action.to_sym)
+
+        next if result.nil? || !result.is_a?(Result)
+
+        case result
+        when Faliure
+          result.faliure_on = action
+          result.finish_on = action
+          break result
+        when Success
+          result.finish_on = action
+        end
+
+        return result
+      end
+    end
+
+    def faliure(hash)
+      Faliure.new hash
+    end
+
+    def success(hash)
+      Success.new hash
+    end
+  end
+end
---
+++ spec/lib/action_service/execution_spec.rb
@@ -0,0 +1,66 @@
+# frozen_string_literal: true
+
+RSpec.describe ActionService::Execution do
+  let(:dummy_class) do
+    Class.new do
+      include ActionService::Execution
+
+      def create_user
+        faliure error: true
+      end
+
+      def send_email
+        success success: true
+      end
+    end
+  end
+  let(:instance) { dummy_class.new }
+
+  describe '#execute' do
+    let(:instance) { dummy_class.new }
+
+    before do
+      allow(instance).to receive(:create_user).and_call_original
+    end
+
+    it 'has to execute list of informed actions' do
+      instance.execute [:create_user]
+
+      expect(instance).to have_received(:create_user)
+    end
+
+    context 'when action return a Faliure instance' do
+      it 'has to fill faliure_on with action' do
+        result = instance.execute [:create_user]
+
+        expect(result.faliure_on).to eq(:create_user)
+      end
+
+      it 'has to fill finish_on with action' do
+        result = instance.execute [:create_user]
+
+        expect(result.finish_on).to eq(:create_user)
+      end
+    end
+
+    context 'when action return a Success instance' do
+      it 'has to fill finish_on with action' do
+        result = instance.execute [:send_email]
+
+        expect(result.finish_on).to eq(:send_email)
+      end
+    end
+  end
+
+  describe '#faliure' do
+    it 'has to return a Faliure instance' do
+      expect(instance.faliure({})).to be_instance_of ActionService::Faliure
+    end
+  end
+
+  describe '#success' do
+    it 'has to return a Success instance' do
+      expect(instance.success({})).to be_instance_of ActionService::Success
+    end
+  end
+end
